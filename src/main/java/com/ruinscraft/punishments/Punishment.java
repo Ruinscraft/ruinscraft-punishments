d2e85809f33dd735d73a97e0be7122251938a618
Dexton Anderson <contact@dextonanderson.com>
2019-06-20 20:11:18 -0500

    add command

--- src/main/java/com/ruinscraft/punishments/Punishment.java
+++ src/main/java/com/ruinscraft/punishments/Punishment.java
@@ -7,7 +7,34 @@ public class Punishment {
     private int punishmentId;
     private UUID punisher;
     private String offender; // UUID, IP, etc
-    private String reason;
     private long duration;
+    private String reason;
 
+    public Punishment(UUID punisher, String offender, long duration, String reason) {
+        this.punisher = punisher;
+        this.offender = offender;
+        this.duration = duration;
+        this.reason = reason;
+    }
+
+    public int getPunishmentId() {
+        return punishmentId;
+    }
+
+    public UUID getPunisher() {
+        return punisher;
+    }
+
+    public String getOffender() {
+        return offender;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+    
 }
--- src/main/java/com/ruinscraft/punishments/commands/PunishmentCommand.java
+++ src/main/java/com/ruinscraft/punishments/commands/PunishmentCommand.java
@@ -1,9 +1,84 @@
 package com.ruinscraft.punishments.commands;
 
+import com.ruinscraft.punishments.Punishment;
+import com.ruinscraft.punishments.PunishmentType;
+import com.ruinscraft.punishments.dispatcher.PunishmentDispatcher;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-public abstract class PunishmentCommand implements CommandExecutor {
+import java.util.UUID;
 
-    private boolean durationAccepted;
+public class PunishmentCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        boolean temporary = label.toLowerCase().startsWith("temp");
+
+        PunishmentType type = null;
+
+        for (PunishmentType _type : PunishmentType.values()) {
+            if (label.toLowerCase().contains(_type.name())) {
+                type = _type;
+            }
+        }
+
+        if (type == null) {
+            return false; // should never happen...
+        }
+
+        switch (args.length) {
+            case 2:
+                if (temporary) {
+                    return showHelp(sender, label, true);
+                } else {
+                    return handlePunishment(sender, args, type, false);
+                }
+            case 3:
+                if (!temporary) {
+                    return showHelp(sender, label, false);
+                } else {
+                    return handlePunishment(sender, args, type, true);
+                }
+            default:
+                return showHelp(sender, label, temporary);
+        }
+    }
+
+    private boolean showHelp(CommandSender sender, String label, boolean temporary) {
+        String help = "/" + label + " <player/ip> " + (temporary ? "" : "<duration>") + " <reason>";
+        sender.sendMessage(help);
+        return true;
+    }
+
+    private boolean handlePunishment(CommandSender sender, String args[], PunishmentType type, boolean temporary) {
+        String offender = args[0];
+        long duration = -1L;
+        final String reason;
+
+        if (temporary) {
+            try {
+                duration = Long.parseLong(args[1]);
+                reason = args[2];
+            } catch (NumberFormatException e) {
+                sender.sendMessage("Invalid duration.");
+                return false;
+            }
+        } else {
+            reason = args[1];
+        }
+
+        UUID punisher = new UUID(0,0);
+
+        if (sender instanceof Player) {
+            punisher = ((Player) sender).getUniqueId();
+        }
+
+        Punishment punishment = new Punishment(punisher, offender, duration, reason);
+        PunishmentDispatcher.dispatch(type, punishment);
+
+        return true;
+    }
 
 }
