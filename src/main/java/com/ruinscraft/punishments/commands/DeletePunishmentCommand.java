68e664c602bc6f281251319cd510660558f281cb
Dexton Anderson <contact@dextonanderson.com>
2021-07-16 17:36:24 -0500

    Implement /pdel

--- src/main/java/com/ruinscraft/punishments/commands/DeletePunishmentCommand.java
+++ src/main/java/com/ruinscraft/punishments/commands/DeletePunishmentCommand.java
@@ -1,16 +1,31 @@
 package com.ruinscraft.punishments.commands;
 
+import com.ruinscraft.punishments.PunishmentsPlugin;
+import com.ruinscraft.punishments.util.Messages;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class DeletePunishmentCommand extends PunishmentCommandExecutor {
-
-    public DeletePunishmentCommand() {
-        super(true, false);
-    }
+public class DeletePunishmentCommand implements CommandExecutor {
 
     @Override
-    protected boolean run(CommandSender sender, String label, String[] args) {
-        sender.sendMessage("Not implemented yet");
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(Messages.COLOR_WARN + "/" + label + " <punishmentid>");
+            return true;
+        }
+
+        int pId;
+
+        try {
+            pId = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            sender.sendMessage(Messages.COLOR_WARN + "Invalid punishmentid");
+            return true;
+        }
+
+        PunishmentsPlugin.get().getStorage().delete(pId);
+        sender.sendMessage(Messages.COLOR_MAIN + "Punishment with id " + pId + " has been deleted (if it existed).");
         return true;
     }
 
